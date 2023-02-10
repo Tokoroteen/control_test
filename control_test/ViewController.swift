118863768c2c265caa2b90b2836765dceb633b43
Tokoroteen <kenta.orbiter@gmail.com>
2023-02-10 15:47:03 +0900

    2-14

--- control_test/ViewController.swift
+++ control_test/ViewController.swift
@@ -9,56 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var number: Int = 1
-    @IBOutlet var countLabel : UILabel!
-    
-    //配列の定義！　[ ]で囲むことに注意！
-    var nameArray: [String] = ["イチロー","ジロー","サブロー"]
+    var number = 0
+    @IBOutlet var countLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let ud = UserDefaults.standard
+        number = ud.integer(forKey: "count")
+        countLabel.text = String(number)
         // Do any additional setup after loading the view.
     }
-
+    
     @IBAction func plus(){
-        number = number + 1
+        number += 1
         countLabel.text = String(number)
         
-//        if number==10 {
-//            countLabel.textColor = UIColor.red
-//        }else if number > 20 && number < 30 {
-//            countLabel.textColor = UIColor.blue
-//        }else if number >= 30 {
-//            countLabel.textColor = UIColor.green
-//        }else{
-//            countLabel.textColor = UIColor.black
-//        }
-        
-        switch number {
-        case 10:
-            countLabel.textColor = UIColor.red
-        case 20:
-            countLabel.textColor = UIColor.blue
-        default:
-            break
-        }
+        //UserDefaultsを使いますよの合図
+        let ud = UserDefaults.standard
+        ud.set(number, forKey: "count")
     }
     
-    @IBAction func fooooo(){
-        print(nameArray[1])
-//        for i in 0..<5 {
-//            print(i)
-//        }
-//        while number<5 {
-//            number=number*2
-//            print(number)
-//        }
-        for name in nameArray{
-            print(name)
-            if name=="ジロー"{
-                print("ジローがいた！！")
-            }
-        }
-    }
+
+    
 }
 
